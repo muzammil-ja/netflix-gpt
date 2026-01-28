962571da54040355644ebc0a6dbd48583a2b5e08
Muzammil <ma7585401@gmail.com>
2026-01-28 14:25:00 +0530

    fix warning

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,16 +1,26 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import {addNowPlayingMovies} from "../utils/moviesSlice";
-const useNowPlayingMovies = ()=>{
-     const dispatch=useDispatch();
-     const nowPlayingMovies=useSelector(store=> store.movies.nowPlayingMovies)
-    const getNowPlayingMovies= async ()=> {
-    const response=await fetch( "https://api.watchmode.com/v1/releases/?apiKey=WObdFY8joporhJdHDg2ml7DTcCItv9D56sE3q9eR&types=movie&regions=IN&limit=20");
-        const json=await response.json();
-        dispatch(addNowPlayingMovies(json.releases));
-    };
-    useEffect(()=>{
-        !nowPlayingMovies && getNowPlayingMovies();
-    },[])
-}
+import { useEffect, useCallback } from "react";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
+
+const useNowPlayingMovies = () => {
+  const dispatch = useDispatch();
+  const nowPlayingMovies = useSelector(
+    (store) => store.movies.nowPlayingMovies
+  );
+
+  const getNowPlayingMovies = useCallback(async () => {
+    const response = await fetch(
+      "https://api.watchmode.com/v1/releases/?apiKey=WObdFY8joporhJdHDg2ml7DTcCItv9D56sE3q9eR&types=movie&regions=IN&limit=20"
+    );
+    const json = await response.json();
+    dispatch(addNowPlayingMovies(json.releases));
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!nowPlayingMovies) {
+      getNowPlayingMovies();
+    }
+  }, [nowPlayingMovies, getNowPlayingMovies]);
+};
+
 export default useNowPlayingMovies;
