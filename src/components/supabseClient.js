f4486ed2587fa9ea7995d5396c3d6285212b5546
Muzammil <ma7585401@gmail.com>
2026-03-02 21:39:19 +0530

    key updated

--- src/components/supabseClient.js
+++ src/components/supabseClient.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://vjjoaalvnplfwxlmpwki.supabase.co";
 const supabaseAnonKey =
-  "sb_publishable_Mkk32bgaepj2oqUlzgoiNA_036fI5le";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqam9hYWx2bnBsZnd4bG1wd2tpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyMTcxNDIsImV4cCI6MjA4MTc5MzE0Mn0.W_JDm2-OqUYNq-dNckeuStak7-JzYGlggyMfJVyrIKw";
 
 export const supabase = createClient(
   supabaseUrl,
