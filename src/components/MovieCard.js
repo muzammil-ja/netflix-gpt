73c7dea1759d825059c195f0e04396b9a4b6f332
Muzammil <ma7585401@gmail.com>
2026-03-03 21:42:39 +0530

    error fix

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,12 +1,18 @@
 import { IMG_CDN_URL } from "../utils/constants";
 
+const MovieCard = ({ posterPath }) => {
+  if (!posterPath) return null;
 
-const MovieCard = ({posterPath}) =>{
-    if(!posterPath) return null;
-    return(
-        <div className="w-36 md:w-48 pr-4">
-            <img alt="Movie Card" src={IMG_CDN_URL+posterPath}/>
-        </div>
-    )
-}
+  return (
+    <div className="w-36 md:w-48 pr-4">
+      <img
+        alt="Movie Card"
+        src={posterPath.startsWith("http") 
+          ? posterPath 
+          : IMG_CDN_URL + posterPath}
+      />
+    </div>
+  );
+};
+
 export default MovieCard;
--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,17 +1,21 @@
 import { useSelector } from "react-redux";
 import MovieList from "./MovieList";
 
-const SecondaryContainer = ()=>{
-    const movies=useSelector((store)=>store.movies)
-    return (
+const SecondaryContainer = () => {
+  const movies = useSelector((store) => store.movies);
+
+  if (!movies?.nowPlayingMovies?.length) return null;   // 👈 ye line add karo
+
+  return (
     <div className="bg-black">
       <div className="mt-0 md:-mt-36 pl-4 md:pl-12 relative z-20">
-        <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
-        <MovieList title={"Popular"} movies={movies.nowPlayingMovies}/>
-        <MovieList title={"Up coming"} movies={movies.nowPlayingMovies}/>
-        <MovieList title={"Trending"} movies={movies.nowPlayingMovies}/>
+        <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
+        <MovieList title="Popular" movies={movies.nowPlayingMovies} />
+        <MovieList title="Up Coming" movies={movies.nowPlayingMovies} />
+        <MovieList title="Trending" movies={movies.nowPlayingMovies} />
       </div>
     </div>
-    )
+  );
 };
+
 export default SecondaryContainer;
